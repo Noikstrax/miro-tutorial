522a7021bfdf8241e8d4012b93bf214727543c46
noikstrax <noikstrax@tuta.io>
2025-07-31 15:00:58 +0300

    react-router-dom setup

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,12 +1,13 @@
-export { BoardCard } from "./board-card";
 import { ROUTES } from "@/shared/model/routes";
-import { Link, href } from "react-router-dom";
+import { href, Link } from "react-router-dom";
 
-export function BoardsListPage() {
+function BoardsListPage() {
   return (
     <div>
-      Boards list
+      <h1>Boards list</h1>
       <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
     </div>
   );
 }
+
+export const Component = BoardsListPage;
