daf0a22c50ac9ef1512741900d9777e8162c2440
noikstrax <noikstrax@tuta.io>
2025-07-31 20:53:39 +0300

    feat(ui): add shadcn, tailwindcss, update boards-list-page component

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,6 +1,8 @@
 import { rqClient } from "@/shared/api/instance";
 import { CONFIG } from "@/shared/model/config";
 import { ROUTES } from "@/shared/model/routes";
+import { Button } from "@/shared/ui/kit/button";
+import { Card, CardFooter, CardHeader } from "@/shared/ui/kit/card";
 import { useQueryClient } from "@tanstack/react-query";
 import { href, Link } from "react-router-dom";
 
@@ -26,7 +28,7 @@ function BoardsListPage() {
     }
   );
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <h1>Boards list {CONFIG.API_BASE_URL}</h1>
       <form
         onSubmit={(e) => {
@@ -42,23 +44,32 @@ function BoardsListPage() {
           Create Board
         </button>
       </form>
-      {boardQuery.data?.map((board) => (
-        <div key={board.id}>
-          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
-            {board.name}
-          </Link>
-          <button
-            disabled={deleteBoardMutation.isPending}
-            onClick={() =>
-              deleteBoardMutation.mutate({
-                params: { path: { boardId: String(board.id) } },
-              })
-            }
-          >
-            Delete
-          </button>
-        </div>
-      ))}
+      <div className="grid grid-cols-3 gap-4">
+        {boardQuery.data?.map((board) => (
+          <Card key={board.id}>
+            <CardHeader>
+              <Button asChild variant={"link"}>
+                <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+                  {board.name}
+                </Link>
+              </Button>
+            </CardHeader>
+            <CardFooter>
+              <Button
+                variant={"destructive"}
+                disabled={deleteBoardMutation.isPending}
+                onClick={() =>
+                  deleteBoardMutation.mutate({
+                    params: { path: { boardId: String(board.id) } },
+                  })
+                }
+              >
+                Delete
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
